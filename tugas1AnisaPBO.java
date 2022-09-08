cc79a92e8c58f231b1334844cba725201980e4f7
AnisatulLatifah <92152235+AnisatulLatifah@users.noreply.github.com>
2022-09-08 20:15:04 +0700

    Add files via upload

---
+++ tugas1AnisaPBO.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class tugas1AnisaPBO {
+    public static void main(String[] args){
+//        int array[] = {2,1,4,1,7,6,0,0,8};
+//        for (int i = 0; i <= 6; i++) {
+//           for (int j = i; j <= 5; j++) {
+//             System.out.print(" ");
+//        }
+//            for (int k = 0; k <= (i * 2) - 2; k++) {
+//             System.out.print("1");
+//        }
+//             System.out.println();
+//        }
+    int array[] = {2,1,4,1,7,6,0,0,8};
+    for (int i : array) {
+        System.out.println(i);
+    }
+//    int i = 1;
+//    do {
+//        System.out.println(i);
+//    }while (++i <= array.length);
+//        System.out.println("looping selesai");
+    }
+}
---
+++ tugas2AnisaPBO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class tugas2AnisaPBO {
+    //Anisatul Latifah 2141762008/04
+   public static void main(String args[]){
+    int[] nim = {2,1,4,1,7,6,2,0,0,8};
+      int total = 0;
+      //Perulangan dengan for
+      for( int num : nim) {
+          total = total+num;
+      }
+      System.out.println("Jumlah NIM adalah :"+total);
+   }
+   }
+  
+   
