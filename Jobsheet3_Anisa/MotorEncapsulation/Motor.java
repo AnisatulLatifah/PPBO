caad86eccd83bede338747650a0a1da554ae164a
AnisatulLatifah <92152235+AnisatulLatifah@users.noreply.github.com>
2022-09-23 08:20:30 +0700

    Add files via upload

---
+++ Jobsheet3_Anisa/MotorEncapsulation/Motor.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MotorEncapsulation;
+public class Motor {
+    private int kecepatan = 0;
+    private boolean kontakOn = false;
+    
+    public void nyalakanMesin(){
+        kontakOn = true;
+    }
+    public void matikanMesin(){
+        kontakOn = false;
+        kecepatan = 0;
+    }
+    public void tambahKecepatan(){
+        if (kontakOn == true){
+            if(kecepatan >= 0 && kecepatan <100){
+            kecepatan += 50;
+            }
+            else if(kecepatan >= 100){
+            kecepatan = 100;
+            System.out.println("Kecepatan tidak bisa bertambah karena Melampaui batas maksimum ! \n");
+            }
+        else{
+            System.out.println("Kecepatan tidak bisa bertambah karena Mesin Off! \n");
+        }
+    }
+    }
+    public void kurangiKecepatan(){
+        if (kontakOn == true) {
+            kecepatan -= 5;
+        }
+        else{
+            System.out.println("Kecepatan tidak bisa berkurang karena Mesin Off \n");
+        }
+    }
+    public void printStatus(){
+        if (kontakOn == true){
+            System.out.println("Kontak On");
+        }
+        else{
+            System.out.println("Kontak Off");
+        }
+        System.out.println("Kecepatan " + kecepatan+ "\n");
+    }
+}
