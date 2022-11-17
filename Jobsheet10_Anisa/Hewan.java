41e855dd2c5acb3b52239f4b59b3f3065d065f55
AnisatulLatifah <92152235+AnisatulLatifah@users.noreply.github.com>
2022-11-17 14:58:33 +0700

    Add files via upload

---
+++ Jobsheet10_Anisa/Hewan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Praktikum1;
+public abstract class Hewan {
+    private int umur;
+    
+    protected Hewan(){
+        this.umur = 0;
+    }
+    public void bertambahUmur(){
+        this.umur += 1;
+    }
+    public abstract void bergerak();
+}
